45301b8824aa44e78e0ce3365d43c1e63c3797e1
YiNANzhang <zhangyinan@ict.ac.cn>
2016-06-04 14:30:38 +0800

    Q_OBJECT error

--- dialog.cpp
+++ dialog.cpp
@@ -1,11 +1,88 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 
-Dialog::Dialog(QDialog *parent) :
+#include <QErrorMessage>
+
+Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
+ //
+
     ui->setupUi(this);
+
+    //initial Table list
+    QStringList strings;
+    strings << "calcforce" << "gpsfpd" << "gpsimu" << "inclinometer" << "statistics" << "warning" << "wgpacurrentspeed";
+    strings << "wgpawave" << "windsensor";
+    iniTableList(0, strings);
+
+    strings.clear();
+
+    //QStringList strings2;
+    strings << "bar" << "enlight1" << "enlight2" << "enlight3" << "enlight4" << "enlight5" << "enlight6" << "enlight7";;
+    strings << "enlight8" << "fbg" << "foxboro" << "gps" << "inclinometer1" << "inv12" << "inv4" << "spm" << "warnings";
+    strings << "wgpacurrentspeed" << "wgpawave" << "windsensor";
+    iniTableList(1, strings);
+
+    strings.clear();
+
+    strings << "calcforce" << "gpsfpd" << "gpsimu" << "heave" << "inclinometer" << "statistics" << "temperature" << "warning";
+    strings << "waveradar" << "waveradarsp" << "windsensor";
+    strings.clear();
+    iniTableList(2, strings);
+
+    ui->comboBox_database->setCurrentIndex(1);
+    int database = ui->comboBox_database->currentIndex();
+    setDataTable(database);
+
+ //   connect(ui->comboBox_database, SIGNAL(currentIndexChanged(int)), this, SIGNAL(comboboxDatabase_changed(int) ) );
+    connect(ui->comboBox_database, SIGNAL( currentIndexChanged(int) ), this, SLOT( setDataTable(int) ) );
+}
+
+void Dialog::iniTableList(int index, QStringList strings)
+{
+    switch (index) {
+    case 0:
+        table111 = strings;
+        break;
+    case 1:
+        table112 = strings;
+        break;
+    case 2:
+        table118 = strings;
+        break;
+    default:
+        break;
+    }
+}
+/*
+void Dialog::comboboxDatabase_changed(int)
+{
+    connect(ui->comboBox_database, currentIndexChanged, this, );
+}
+*/
+void Dialog::setDataTable( int style )
+{
+    if ( style == FPSO111 )
+    {
+        ui->comboBox_dababaseT->clear();
+        ui->comboBox_dababaseT->addItems(table111);
+    } else if ( style == FPSO112 )
+    {
+        ui->comboBox_dababaseT->clear();
+        ui->comboBox_dababaseT->addItems(table112);
+    } else if ( style == FPSO118 )
+    {
+        ui->comboBox_dababaseT->clear();
+        ui->comboBox_dababaseT->addItems(table118);
+    } else {
+        QErrorMessage *errDialog = new QErrorMessage(this);
+        errDialog->showMessage("The database is not exist!");
+        return;
+    }
+    ui->comboBox_dababaseT->setCurrentIndex(0);
+    repaint();
 }
 
 Dialog::~Dialog()
--- dialog.h
+++ dialog.h
@@ -1,8 +1,9 @@
 #ifndef PREPROCESSWIDGET_H
 #define PREPROCESSWIDGET_H
 
-#include <QWidget>
 #include <QDialog>
+class QWidget;
+class QStringList;
 
 namespace Ui {
 class Dialog;
@@ -10,12 +11,36 @@ class Dialog;
 
 class Dialog : public QDialog
 {
+//    Q_OBJECT
+
 public:
-    explicit Dialog(QDialog *parent = 0);
+    enum DataBase
+    {
+        FPSO111,
+        FPSO112,
+        FPSO118
+    };
+/*
+signals:
+    void comboboxDatabase_changed(int);
+*/
+public slots:
+    void setDataTable( int );
+
+
+public:
+    Dialog(QWidget *parent = 0);
     ~Dialog();
+
+    void iniTableList(int index, QStringList strings);
 
 private:
     Ui::Dialog *ui;
+
+    QStringList table111;
+    QStringList table112;
+    QStringList table118;
+
 };
 
 #endif // PREPROCESSWIDGET_H
